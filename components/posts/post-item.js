d95484833a8002b9bde50e29c057d05f55da488f
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-06-13 23:43:42 +0530

    :zap: :art: :lipstick: Post Grid and item component

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -1,6 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+import classes from './post-item.module.css'
 
+export default function PostItem({ post }) {
+  const { title, slug, image, date, excerpt } = post;
 
-export default function PostItem() {
-  
-  return <div></div>
+  const formattedDate = new Date(date).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  });
+
+  const imagePath = `/images/posts/${slug}/${image}`
+  return <li className={classes.post}>
+    <Link>
+      <div className={classes.image}>
+        <Image src={imagePath} alt={title} height={200} width={300} />
+      </div>
+      <div className={classes.content}>
+        <h3>{title}</h3>
+        <time>{formattedDate}</time>
+        <p>{excerpt}</p>
+
+      </div>
+    </Link>
+  </li>
 }

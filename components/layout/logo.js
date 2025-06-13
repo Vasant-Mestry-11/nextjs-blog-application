730ee5a12382b3556098fd59bb5e39331f5d51e3
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-06-13 22:13:37 +0530

    :zap: :lipstick: logo and main navigation compoent data added

--- components/layout/logo.js
+++ components/layout/logo.js
@@ -1,5 +1,4 @@
-
-
+import classes from './logo.module.css'
 export default function Logo() {
-  return <div>Logo</div>
+  return <div className={classes.logo}>Vasant&apos;s Blog</div>
 }
--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import Logo from "./logo";
+import classes from './main-navigation.module.css';
 
 export default function MainNavigation() {
-  return <header>
+  return <header className={classes.header}>
     <Link href="/">
       <Logo />
     </Link>
@@ -10,6 +11,8 @@ export default function MainNavigation() {
     <ul>
       <li>
         <Link href="/posts">Posts</Link>
+      </li>
+      <li>
         <Link href="/contact">Contact</Link>
       </li>
     </ul>

cc51688e5c3f13133096945033eb82d2845beee1
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-06-15 15:31:19 +0530

    :zap: :art: single post fetch using static props and static paths

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -3,16 +3,8 @@ import PostHeader from "./post-header";
 import classes from './post-content.module.css'
 import Markdown from "react-markdown";
 
-const DUMMY_ARTICLE = {
-  title: "Getting started with nextjs",
-  slug: "getting-started-with-nextjs",
-  image: "getting-started-with-nextjs.png",
-  date: "2022-02-10",
-  content: "# Nextjs is react framwork",
-}
-
-export default function PostContent() {
-  const { title, image, slug, content } = DUMMY_ARTICLE;
+export default function PostContent({ post }) {
+  const { title, image, slug, content } = post;
   const imagePath = `/images/posts/${slug}/${image}`
   return <article className={classes.content}>
     <PostHeader title={title} image={imagePath} />
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,34 @@
 import PostContent from "@/components/posts/post-detail/post-content"
+import { getPostsData, getPostsFiles } from "@/lib/post-utils";
 
-const SinglePostPage = () => {
-  return <PostContent />
+const SinglePostPage = ({ post }) => {
+  return <PostContent post={post} />
 }
 
 export default SinglePostPage
+
+
+export function getStaticPaths() {
+
+  const postFileNames = getPostsFiles();
+  const slugs = postFileNames.map((fileName) => fileName.replace(/\.md/, ''));
+
+  return {
+    paths: slugs.map((slug) => ({ params: { slug } })),
+    fallback: false
+  }
+}
+
+export function getStaticProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getPostsData(slug)
+
+  return {
+    props: {
+      post: postData
+    },
+    revalidate: 600,
+  }
+}

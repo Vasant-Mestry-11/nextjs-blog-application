851e527a7e0f27f5126dc29334abce9c80b54bbe
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-06-16 17:57:03 +0530

    :zap: Loading image (not rendering the images for me)

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,15 +1,52 @@
 import PostHeader from "./post-header";
 
-import classes from './post-content.module.css'
+import classes from "./post-content.module.css";
 import Markdown from "react-markdown";
+import Image from "next/image";
 
 export default function PostContent({ post }) {
   const { title, image, slug, content } = post;
-  const imagePath = `/images/posts/${slug}/${image}`
-  return <article className={classes.content}>
-    <PostHeader title={title} image={imagePath} />
-    <Markdown>
-      {content}
-    </Markdown>
-  </article>
+
+  const customRenderers = {
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+
+        console.log(slug)
+
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${slug}/${image.properties.src}`}
+              alt={image.properties.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{paragraph.children}</p>
+    },
+  };
+  const imagePath = `/images/posts/${slug}/${image}`;
+  return (
+    <article className={classes.content}>
+      <PostHeader title={title} image={imagePath} />
+      <Markdown components={customRenderers}>{content}</Markdown>
+    </article>
+  );
 }
--- components/posts/post-detail/post-header.js
+++ components/posts/post-detail/post-header.js
@@ -4,7 +4,6 @@ import classes from './post-header.module.css';
 export default function PostHeader({
   title, image
 }) {
-
   return <header className={classes.header}>
     <h1>{title}</h1>
     <Image src={image} alt={title} width={200} height={150} />

43d8ecd00cbd216ac2c407291f5fba86ac592f65
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-06-20 21:27:44 +0530

    :zap: :art: contact form api integration

--- components/contact-form/contact-form.js
+++ components/contact-form/contact-form.js
@@ -1,29 +1,85 @@
-import classes from './contact-form.module.css'
+import { useState } from "react";
+import classes from "./contact-form.module.css";
 
 function ContactForm() {
+  const [formData, setFormData] = useState({
+    email: "",
+    name: "",
+    message: "",
+  });
 
-  return <section className={classes.contact}>
-    <h1>How can I help you?</h1>
-    <form className={classes.form}>
-      <div className={classes.controls}>
-        <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input id='email' type='email' required />
+  function sendMessageHandler(event) {
+    const { email, name, message } = formData;
+
+    event.preventDefault();
+
+    fetch("/api/contact", {
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        name,
+        message,
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
+
+  function changeHandler(event) {
+    setFormData((prevData) => ({
+      ...prevData,
+      [event.target.name]: event.target.value,
+    }));
+  }
+
+  const { email, name, message } = formData;
+
+  return (
+    <section className={classes.contact}>
+      <h1>How can I help you?</h1>
+      <form className={classes.form} onSubmit={sendMessageHandler}>
+        <div className={classes.controls}>
+          <div className={classes.control}>
+            <label htmlFor="email">Your Email</label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              required
+              value={email}
+              onChange={changeHandler}
+            />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="name">Your Name</label>
+            <input
+              id="name"
+              type="text"
+              name="name"
+              required
+              value={name}
+              onChange={changeHandler}
+            />
+          </div>
         </div>
         <div className={classes.control}>
-          <label htmlFor='name'>Your Name</label>
-          <input id='name' type='text' required />
+          <label htmlFor="message">Your Message</label>
+          <textarea
+            id="message"
+            rows="5"
+            required
+            name="message"
+            value={message}
+            onChange={changeHandler}
+          ></textarea>
         </div>
-      </div>
-      <div className={classes.control}>
-        <label htmlFor='message'>Your Message</label>
-        <textarea id='message' rows='5' />
-      </div>
-      <div className={classes.actions}>
-        <button>Send Message</button>
-      </div>
-    </form>
-  </section>
+        <div className={classes.actions}>
+          <button>Send Message</button>
+        </div>
+      </form>
+    </section>
+  );
 }
 
-export default ContactForm
+export default ContactForm;
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,7 +1,7 @@
 function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
-
+    console.log(email, name, message)
     if (
       !email ||
       !email.includes("@") ||

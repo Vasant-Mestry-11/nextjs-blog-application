43fddea27dc29728f8b926dc394fe554472a7d54
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-06-20 21:03:00 +0530

    :zap: :art: added api handler for contact form

---
+++ pages/api/contact.js
@@ -0,0 +1,34 @@
+function handler(req, res) {
+  if (req.method === "POST") {
+    const { email, name, message } = req.body;
+
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      !name.trim() ||
+      !message ||
+      !message.trim()
+    ) {
+      res.status(422).json({
+        message: "Invalid Input",
+      });
+      return;
+    }
+
+    // store in db
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
+
+    console.log(newMessage);
+
+    res.status(201).json({
+      message: "Successfully added message",
+    });
+  }
+}
+
+export default handler;

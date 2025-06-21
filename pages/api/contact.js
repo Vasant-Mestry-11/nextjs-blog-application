1b9d087334120c5b4d8ff01adc3ce0e940ac98bc
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2025-06-21 12:30:02 +0530

    :zap: :art: :package: :cloud: dotenv, added message to database

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,7 +1,11 @@
-function handler(req, res) {
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
-    console.log(email, name, message)
     if (
       !email ||
       !email.includes("@") ||
@@ -16,17 +20,38 @@ function handler(req, res) {
       return;
     }
 
-    // store in db
+    let client;
+    try {
+      client = await MongoClient.connect(process.env.MONGO_CONNECTION_URL);
+    } catch (error) {
+      res.status(500).json({ message: "Failed to connect Database" });
+      return;
+    }
+
     const newMessage = {
       email,
       name,
       message,
     };
 
-    console.log(newMessage);
+    const db = client.db("blog-site");
+
+    try {
+      const result = await db.collection("message").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({
+        message: "Failed to add message",
+      });
+      return;
+    }
+
+    client.close();
 
     res.status(201).json({
       message: "Successfully added message",
+      data: newMessage,
     });
   }
 }
